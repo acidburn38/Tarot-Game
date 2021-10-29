76c70cf07c8ec137369c146f7af677cc186e20e1
Laure <lacomtessebathory@gmail.com>
2021-10-29 14:43:35 +0200

    js lists

---
+++ script.js
@@ -0,0 +1,5 @@
+let file_name = ['KingofCups.jpg', 'KingOfPentacles.jpg', 'KingOfSwords.jpg', 'KingOfWand.jpg', 'KnightofCups.jpg', 'KnightOfPentacle.jpg', 'KnightOfSwords.jpg', 'KnightOfWands.jpg', 'PageOfCups.jpg', 'PageOfPentacles.jpg', 'PageOfSwords.jpg', 'PageOfWands.jpg', 'QueenofCups.jpg', 'QueenOfPentacles.jpg', 'QueenOfSwords.jpg', 'QueenOfWands.jpg'];
+
+let card_name = ['King of Cups', 'King Of Pentacles', 'King Of Swords', 'King Of Wand', 'Knight of Cups', 'Knight Of Pentacle', 'Knight Of Swords', 'Knight Of Wands', 'Page Of Cups', 'Page Of Pentacles', 'Page Of Swords', 'Page Of Wands', 'Queen of Cups', 'Queen Of Pentacles', 'Queen Of Swords', 'Queen Of Wands'];
+
+let info_card = [];
